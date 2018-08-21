e0cfcacea848eb9f3319ab95b454b259aa211b32
sunbl5 <sunbl5@asiainfo.com>
2018-08-21 20:15:48 +0800

    Update Java1222

--- Hello.java
+++ Hello.java
@@ -2,6 +2,7 @@ public class Hello
 {
 	public static void main(String[] args)
 	{
-		System.out.println("世界你好！");
+		System.out.println("Hello Java222!");
+		System.out.println("Hello Java222!");
 	}
 }
