928f904a1376ea4b6c88b9d8f9fc1378900bcaf8
sunbl5 <sunbl5@asiainfo.com>
2018-08-28 20:49:37 +0800

    simple print

--- Hello.java
+++ Hello.java
@@ -1,15 +1,8 @@
 public class Hello
 {
-	public static final String COMPANY_NAME = 'asiainfo';
 	public static void main(String[] args)
 	{
-		System.out.println("Hello World!");
-		System.out.println("Hello GIT!");
-		print();
-	}
-	public static void print(){
-		System.out.println("*********************");
-		System.out.println("*    Hello World!   *");
-		System.out.println("*********************");
+		System.out.println("1111111111111111111");
+		System.out.println("2222222222222222222");
 	}
 }
