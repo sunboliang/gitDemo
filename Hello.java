4a0204f3f8a8650963865bf54afdcfb94248f682
sunbl5 <sunbl5@asiainfo.com>
2018-08-28 20:52:52 +0800

    hello print

--- Hello.java
+++ Hello.java
@@ -2,7 +2,7 @@ public class Hello
 {
 	public static void main(String[] args)
 	{
-		System.out.println("1111111111111111111");
-		System.out.println("2222222222222222222");
+		System.out.println("Hello World!!!");
+		System.out.println("Hello Git!!!!!");
 	}
 }
